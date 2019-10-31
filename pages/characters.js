5dff3a5d9db53bc98e1ec6f7ba85dcb19e919107
Nathalej <nathanbarcos12@gmail.com>
2019-10-31 17:29:01 -0500

    Adding Characters component page and fixing bug in a Comics component page

--- pages/characters.js
+++ pages/characters.js
@@ -2,17 +2,17 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 
-const Comics = () => (
+const Characters = () => (
   <div>
     <Head>
-      <title>Comics | Marvel Catalog</title>
+      <title>Characters | Marvel Catalog</title>
       <link rel='icon' href='/favicon.ico' />
     </Head>
     <Nav />
-    <div className='comics'>
+    <div className='characters'>
       <h1 className='title'>Welcome to Marvel Catalog</h1>      
     </div>
   </div>
 )
 
-export default Comics
+export default Characters
